fd46d0f559153cda346c8b4217d2b5f5fe8f080c
Maciej Laskowski <maciek30088@gmail.com>
2023-11-01 19:09:22 +0100

    Add user authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,21 @@ import * as cors from 'cors';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ProductModule,
+    UsersModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Post, Delete, Body, UseGuards, Request, Response } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDTO } from './dtos/RegisterDTO';
+import { LocalAuthGuard } from './local-auth.guard';
+import { JwtAuthGuard } from './jwt-auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/register')
+  register(@Body() registerData: RegisterDTO) {
+    return this.authService.register(registerData);
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Request() req, @Response() res) {
+    const tokens = await this.authService.createSession(req.user);
+    res.cookie('auth', tokens, { httpOnly: true });
+    res.send({
+      message: 'success',
+    });
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete('logout')
+  async logout(@Response() res) {
+    res.clearCookie('auth', { httpOnly: true });
+    res.send({
+      message: 'success',
+    });
+  }
+}
